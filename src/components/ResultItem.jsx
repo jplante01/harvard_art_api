2a806711bbf12d74e007b459ca5dd704f2b732d8
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-06-16 09:59:26 -0700

    Add to ResultItem card

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -1,10 +1,22 @@
 function ResultsItem({ record }) {
+  const century = record.century ? record.century : 'not provided';
+  const title = record.title ? record.title : 'none';
+  const culture = record.culture ? record.culture : 'not provided';
   return (
     <div>
+        <div className="card bg-theme-500 shadow-xl rounded-lg">
       {record.images[0] &&
-        <img src={record.images[0].baseimageurl}></img>
+        <figure className="px-2 pt-2">
+          <img src={record.images[0].baseimageurl}></img>
+        </figure>
       }
-      <h3>Century: {record.century}</h3>
+          <div className="card-body">
+            <h3  className="px-2 pb-2 card-title text-theme-100">{title}</h3>
+            <h3 className="px-2 pb-2 text-theme-50">{culture}</h3>
+            <h3 className="px-2 pb-2 text-theme-50">Century: {century}</h3>
+          </div>
+        </div>
+      
     </div>
   ) 
 }
