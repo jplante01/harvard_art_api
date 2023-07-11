e02f3c016f5961b0eeed8264decb43b96ea93214
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-07-11 07:20:50 -0700

    Persist the api key to local storage

--- src/components/ApiKeyEntryDialog.jsx
+++ src/components/ApiKeyEntryDialog.jsx
@@ -2,12 +2,23 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Dialog } from '@headlessui/react';
 import ApiKeyInput from './ApiKeyInput';
 import { AppContext } from '../contexts/AppContext';
+
 function ApiKeyEntryDialog() {
-  const [apiKeyDialogOpen, setApiKeyDialogOpen] = useState(false);
+  const [apiKeyDialogOpen, setApiKeyDialogOpen] = useState(true);
   const { apiKey, setApiKey } = useContext(AppContext);
+
   useEffect(() => {
-    setApiKey('5e0a18c4-3d19-439b-abd9-7ed55f755093');
+    const storedApiKey = localStorage.getItem('apiKey');
+    if (storedApiKey) {
+      setApiKey(storedApiKey);
+      setApiKeyDialogOpen(false);
+    }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('apiKey', apiKey);
+  }, [apiKey]);
+
   return (
     <Dialog open={apiKeyDialogOpen} onClose={() => setApiKeyDialogOpen(false)}>
       <Dialog.Panel className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
--- src/components/ResultsGrid.jsx
+++ src/components/ResultsGrid.jsx
@@ -8,6 +8,7 @@ function ResultsGrid() {
   return (
     <div className="overflow-auto p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 xl:grid-cols-4">
+        {console.log(records)}
         {records
           && records.map((record) => (
             <ResultsItem key={record.objectid} record={record} />
