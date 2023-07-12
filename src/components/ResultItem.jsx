ae73540a07a27c792f856d98f350e7d0ea691569
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-07-12 12:34:49 -0700

    Syntax changes

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -2,14 +2,14 @@ function ResultsItem({ record }) {
   const century = record.century ? record.century : 'not provided';
   const title = record.title ? record.title : 'none';
   const culture = record.culture ? record.culture : 'not provided';
+  const image = record.primaryimageurl ? record.primaryimageurl : '';
+
   return (
     <div>
       <div className="card bg-secondary shadow-xl rounded-lg">
-        {record.images[0] && (
-          <figure className="px-2 pt-2">
-            <img src={record.images[0].baseimageurl} alt="art" />
-          </figure>
-        )}
+        <figure className="px-2 pt-2">
+          <img src={image} alt="art" />
+        </figure>
         <div className="card-body">
           <h3 className="card-title text-secondary-content">{title}</h3>
           <h3 className="textsecondary-content">{culture}</h3>
