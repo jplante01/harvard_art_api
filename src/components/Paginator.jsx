f7c10f80cc58f51770e3590bc14e83eff1883930
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-07-18 07:05:07 -0700

    Tweak paginator

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,31 +1,26 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Paginator({
-  numPages, activePage, nextPage, prevPage
-}) {
+function Paginator({ numPages, activePage, nextPage, prevPage }) {
   const elements = [];
 
   for (let i = 1; i <= numPages; i++) {
-    elements.push(
-      <button key={uuidv4()} className="join-item btn">
-        {i}
-      </button>
-    );
+    if (activePage === i) {
+      elements.push(
+        <button key={uuidv4()} className="join-item btn btn-active">
+          {i}
+        </button>
+      );
+    } else {
+      elements.push(
+        <button key={uuidv4()} className="join-item btn">
+          {i}
+        </button>
+      );
+    }
   }
   return (
-    <div className="flex flex-row justify-center mb-4">
-      <div className="join">
-        {/* {numbers.map((el, idx) => (
-          <button key={uuidv4()} className="join-item btn">
-            {idx}
-          </button>
-          <button key={pageNumber} className="btn-active join-item btn">
-            {pageNumber}
-          </button>
-
-        ))} */}
-        {elements}
-      </div>
+    <div className="mb-4 flex flex-row justify-center">
+      <div className="join flex-wrap">{elements}</div>
     </div>
   );
 }
